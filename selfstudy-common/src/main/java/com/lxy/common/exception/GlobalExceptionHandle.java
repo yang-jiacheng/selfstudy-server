41e27c3aad73b8a3d147651665a5f5adec224bf4
jiacheng yang <1529425632@qq.com>
2024-12-28 16:29:02 +0800

    1.修改日志策略 2.过滤器打印请求体

--- selfstudy-common/src/main/java/com/lxy/common/exception/GlobalExceptionHandle.java
+++ selfstudy-common/src/main/java/com/lxy/common/exception/GlobalExceptionHandle.java
@@ -1,10 +1,8 @@
-package com.lxy.app.exception;
+package com.lxy.common.exception;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpStatus;
 import com.lxy.common.domain.R;
-import com.lxy.common.util.JsonUtil;
-import com.lxy.common.util.LogUtil;
-import com.lxy.common.vo.ResultVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,36 +21,38 @@ import javax.servlet.http.HttpServletRequest;
  * @Version: 1.0
  */
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandle {
 
     private final static Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandle.class);
 
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
-    @ResponseBody
     public R<Object> methodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request){
-        String msg = "请求方法不支持 "+e.getMethod();
-        String requestURI = request.getRequestURI();
-        LOG.error("{}发生异常_请求方法不支持{}",requestURI,e.getMessage());
+        String msg = StrUtil.format("请求方法不支持 {}, 请求地址为 {}",e.getMethod(),request.getRequestURI());
+        LOG.error(msg,e);
         return R.fail(HttpStatus.HTTP_BAD_METHOD,msg);
     }
 
     @ExceptionHandler({MissingServletRequestParameterException.class})
-    @ResponseBody
     public R<Object> requestParameterException(MissingServletRequestParameterException e, HttpServletRequest request){
-        String requestURI = request.getRequestURI();
-        LOG.error("{}发生异常_请求参数错误{}",requestURI,e.getMessage());
-
-        String msg = "参数类型为 "+e.getParameterType()+" 的必需请求参数 "+ e.getParameterName()+" 不存在";
+        String msg = StrUtil.format("发生异常_请求参数错误,参数类型为 {} 的必需请求参数 {} 不存在, 请求地址为 {}",e.getParameterType(), e.getParameterName(),request.getRequestURI());
+        LOG.error(msg,e);
         return R.fail(HttpStatus.HTTP_BAD_REQUEST,msg);
     }
 
     @ExceptionHandler({Exception.class})
-    @ResponseBody
     public R<Object> handleException(Exception e, HttpServletRequest request){
-        String requestURI = request.getRequestURI();
-        LOG.error("{}发生异常{}",requestURI,e);
-        String msg = "无法与服务器建立安全连接";
+        String msg = StrUtil.format("发生异常,请求地址为 {}",request.getRequestURI());
+        LOG.error(msg,e);
+        String msgRes = "无法与服务器建立安全连接";
+        return R.fail(HttpStatus.HTTP_INTERNAL_ERROR,msgRes);
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    public R<Object> handleNullPointerException(NullPointerException e, HttpServletRequest request){
+        String msg = StrUtil.format("空指针异常,请求地址为 {}",request.getRequestURI());
+        LOG.error(msg,e);
         return R.fail(HttpStatus.HTTP_INTERNAL_ERROR,msg);
     }
+
 }
