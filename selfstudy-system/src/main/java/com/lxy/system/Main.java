d32e9ae1ea0da326942daccff7db1dcee98888fd
jiacheng yang <yjc1529425632@gmail.com>
2025-03-27 21:03:08 +0800

    拆分模块

---
+++ selfstudy-system/src/main/java/com/lxy/system/Main.java
@@ -0,0 +1,25 @@
+package com.lxy.system;
+
+/**
+ * TODO
+ *
+ * @author jiacheng yang.
+ * @version 1.0
+ * @since 2022/12/20 10:49
+ */
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
