f3c8e019db633d3a22dbfef30d793b8abe1e107f
jiacheng yang <yjc1529425632@gmail.com>
2025-05-14 17:47:27 +0800

    spring boot再次升级到3.4.5、集成xxljob

---
+++ selfstudy-admin/src/main/java/com/lxy/admin/task/AdminTask.java
@@ -0,0 +1,36 @@
+package com.lxy.admin.task;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * TODO
+ *
+ * @author jiacheng yang.
+ * @version 1.0
+ * @since 2022/12/20 10:49
+ */
+
+@Slf4j
+@Component
+public class AdminTask {
+
+    /**
+     * 测试admin任务
+     * @author jiacheng yang.
+     * @since 2025/5/9 15:08
+     */
+    @XxlJob("testAdminTask")
+    public void testAdminTask(){
+        log.info("测试admin任务开始");
+        ThreadUtil.sleep(3000);
+        log.info("测试admin任务结束");
+    }
+
+}
---
+++ selfstudy-mobile-app/src/main/java/com/lxy/app/task/MobileAppTask.java
@@ -0,0 +1,33 @@
+package com.lxy.app.task;
+
+import cn.hutool.core.thread.ThreadUtil;
+import com.xxl.job.core.handler.annotation.XxlJob;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO
+ *
+ * @author jiacheng yang.
+ * @version 1.0
+ * @since 2022/12/20 10:49
+ */
+
+@Slf4j
+@Component
+public class MobileAppTask {
+
+    /**
+     * 测试mobile-app任务
+     * @author jiacheng yang.
+     * @since 2025/5/9 15:08
+     */
+    @XxlJob("testMobileAppTask")
+    public void testMobileAppTask(){
+        log.info("测试mobile-app任务开始");
+        ThreadUtil.sleep(3000);
+        log.info("测试mobile-app任务结束");
+    }
+
+}
