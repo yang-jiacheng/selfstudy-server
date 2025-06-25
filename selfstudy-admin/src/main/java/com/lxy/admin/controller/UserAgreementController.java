703c4a1ffe27d7c3f710779f8185982280b68e4c
jiacheng yang <yjc1529425632@gmail.com>
2025-06-25 13:43:32 +0800

    refactor(userAgreement): 隐私政策

--- selfstudy-admin/src/main/java/com/lxy/admin/controller/UserAgreementController.java
+++ selfstudy-admin/src/main/java/com/lxy/admin/controller/UserAgreementController.java
@@ -1,13 +1,16 @@
 package com.lxy.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.lxy.common.annotation.Log;
 import com.lxy.common.domain.R;
 import com.lxy.common.enums.LogBusinessType;
 import com.lxy.common.enums.LogUserType;
+import com.lxy.system.dto.AgreementEditDTO;
 import com.lxy.system.po.UserAgreement;
 import com.lxy.system.service.UserAgreementService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -32,27 +35,23 @@ public class UserAgreementController {
     @Resource
     private UserAgreementService agreementService;
 
-    @PostMapping(value = "/getAgreement", produces = "application/json")
-    public R<Map<String,Object>> getAgreement(){
-        List<UserAgreement> list = agreementService.list();
-        Map<String ,Object> map = new HashMap<>(2);
-        for (UserAgreement agreement : list) {
-            Integer type = agreement.getType();
-            if (type==1){
-                map.put("policy",agreement.getContent());
-            }else {
-                map.put("agreement",agreement.getContent());
-            }
+    @PostMapping(value = "/getAgreement-{type}", produces = "application/json")
+    public R<String> getAgreement(@PathVariable(value = "type") Integer type){
+        LambdaQueryWrapper<UserAgreement> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserAgreement::getType,type);
+        UserAgreement agreement = agreementService.getOne(wrapper);
+        String content = "";
+        if(agreement != null){
+            content = agreement.getContent();
         }
-        return R.ok(map);
+        return R.ok(content);
     }
 
     @Log(title = "修改隐私政策与用户协议", businessType = LogBusinessType.UPDATE, userType = LogUserType.ADMIN)
     @PostMapping(value = "/saveAgreement", produces = "application/json")
-    public R<Object> saveAgreement(@RequestParam(value = "type") Integer type,
-                                @RequestParam(value = "content") String content){
+    public R<Object> saveAgreement(@RequestBody @Valid AgreementEditDTO dto){
         LambdaUpdateWrapper<UserAgreement> wrapper = new LambdaUpdateWrapper<>();
-        wrapper.eq(UserAgreement::getType,type).set(UserAgreement::getContent,content);
+        wrapper.eq(UserAgreement::getType,dto.getType()).set(UserAgreement::getContent,dto.getContent());
         agreementService.update(wrapper);
         return R.ok();
     }
