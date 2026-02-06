53acaf6179e0120f724299a02a1abab5c30d1bdf
yangjiacheng <yjc1529425632@gmail.com>
2026-02-06 17:54:07 +0800

    docs: xxljob初始化sql

--- xxl-job-admin/doc/tables_xxl_job.sql
+++ xxl-job-admin/doc/tables_xxl_job.sql
@@ -5,7 +5,7 @@
 ##CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
 ##use `xxl_job`;
 
-##SET NAMES utf8mb4;
+SET NAMES utf8mb4;
 
 CREATE TABLE `xxl_job_info`
 (
